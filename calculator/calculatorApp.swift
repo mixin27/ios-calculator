e73f94182e3fcd8f280b8c779ccb2db070d49001
Kyu Kyu Thin <kyukyuthin.sbs@gmail.com>
2023-03-20 09:24:17 +0630

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by SYSTEMATIC on 20/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
